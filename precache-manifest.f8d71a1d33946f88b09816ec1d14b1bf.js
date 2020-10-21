6dc4dfe8d2925331791c58d7039c5b9e0f4b1f87
protic <protic80@gmail.com>
2020-10-22 01:15:15 +0200

    Updates

---
+++ precache-manifest.f8d71a1d33946f88b09816ec1d14b1bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b54ad2b74d6bcc7c0ee3e7f8e33d08",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fb2d970fca0b502ce87d",
+    "url": "/monsters-rolodex/static/css/main.08b1d3c1.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb2d970fca0b502ce87d",
+    "url": "/monsters-rolodex/static/js/main.c484bf0d.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
